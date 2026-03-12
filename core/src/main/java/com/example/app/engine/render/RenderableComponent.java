bf906553b24d2da3f2a9b277d6ea9c4a22e80df0
AdGWE <adrielgohsg@gmail.com>
2026-03-12 23:51:56 +0800

    Added enemy,player and entity factory

--- core/src/main/java/com/example/app/engine/render/RenderableComponent.java
+++ core/src/main/java/com/example/app/engine/render/RenderableComponent.java
@@ -5,8 +5,16 @@ import com.example.app.engine.components.Component;
 public final class RenderableComponent implements Component {
 
     public String renderKey;
-
+    public float width;
+    public float height;
+    
     public RenderableComponent(String key) {
         this.renderKey = key;
+    }
+    
+    public RenderableComponent(String renderKey, float width, float height) {
+        this.renderKey = renderKey;
+        this.width = width;
+        this.height = height;
     }
 }
